de55989fa1efc284787f1dccbe976920e181510f
Octavio Lara <octavioasapchi@gmail.com>
2024-05-13 16:16:32 -0400

    ADD tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
-     import { invoke } from '@tauri-apps/api/tauri'
-    let name = '';
-    let greetMsg = ''
-     async function onClick(){
-        greetMsg = await invoke('greet', {name}); 
-     }
+	import { invoke } from '@tauri-apps/api/tauri';
+	let name = '';
+	let greetMsg = '';
+	async function onClick() {
+		greetMsg = await invoke('greet', { name });
+	}
 </script>
 
 <h1>New tauri</h1>
-<input type="text" bind:value={name}>
-<button on:click={onClick}>
-    Saluda
-</button>
+<input type="text" bind:value={name} />
+<button on:click={onClick}> Saluda </button>
 
 {#if greetMsg != ''}
-    <p>
-        {greetMsg}
-    </p>
+	<p>
+		{greetMsg}
+	</p>
 {/if}

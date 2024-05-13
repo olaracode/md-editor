e1fd40834e029527fa8b46029b3cefea0e4427d9
Octavio Lara <octavioasapchi@gmail.com>
2024-05-13 12:14:27 -0400

    INIT tauri app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+     import { invoke } from '@tauri-apps/api/tauri'
+    let name = '';
+    let greetMsg = ''
+     async function onClick(){
+        greetMsg = await invoke('greet', {name}); 
+     }
+</script>
+
+<h1>New tauri</h1>
+<input type="text" bind:value={name}>
+<button on:click={onClick}>
+    Saluda
+</button>
+
+{#if greetMsg != ''}
+    <p>
+        {greetMsg}
+    </p>
+{/if}
